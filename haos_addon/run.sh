35a454963a3ab8e91731f3ad53afc2497389c490
wallus <jimkara@gmail.com>
2024-08-24 14:57:43 +1000

    Tweaks

--- haos_addon/run.sh
+++ haos_addon/run.sh
@@ -1,18 +1,16 @@
-flask run
 #!/usr/bin/env bash
 set -e
 
 # Load Bashio library
 source /usr/lib/bashio/bashio.sh
 
-# Set FLASK_APP environment variable
-export FLASK_APP=app.py
-
-export FLASK_RUN_HOST=0.0.0.0
-
-# Retrieve configuration options
+# Retrieve configuration options using bashio
 OPENAI_API_KEY=$(bashio::config 'OPENAI_API_KEY')
 PORT=$(bashio::config 'port')
+
+# Set FLASK_APP environment variable
+export FLASK_APP=app.py
+export OPENAI_API_KEY=${OPENAI_API_KEY}
 
 # Run the Flask app with the specified port
 flask run --host=0.0.0.0 --port=${PORT}
