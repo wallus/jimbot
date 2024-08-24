d6521f22c126d613e647738df33d8f11fbbf6c2b
wallus <jimkara@gmail.com>
2024-08-24 13:21:34 +1000

    Added the OpenAI key as a parameter.

--- haos_addon/run.sh
+++ haos_addon/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+set -e
+source /usr/lib/bashio/bashio.sh
+OPENAI_API_KEY=$(bashio::config 'OPENAI_API_KEY')
+
 export FLASK_APP=app.py
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=8087
