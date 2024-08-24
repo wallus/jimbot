5a82786a8b270d276b5fb8b6fe87035660608072
wallus <jimkara@gmail.com>
2024-08-24 13:33:22 +1000

    Tweaks to HAOS config

--- haos_addon/run.sh
+++ haos_addon/run.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-
 set -e
-source /usr/lib/bashio/bashio.sh
-OPENAI_API_KEY=$(bashio::config 'OPENAI_API_KEY')
 
+source /usr/lib/bashio/bashio.sh
+
+# Retrieve configuration options
+OPENAI_API_KEY=$(bashio::config 'OPENAI_API_KEY')
+PORT=$(bashio::config 'port')
+
+export OPENAI_API_KEY=$OPENAI_API_KEY
 export FLASK_APP=app.py
-export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=8087
-flask run
+
+flask run --host=0.0.0.0 --port=${PORT}
