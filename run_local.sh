065e5bc19ed5c8aa2d52bcfdd79232cc74612e6b
wallus <jimkara@gmail.com>
2024-08-24 15:26:57 +1000

    Git ignore

--- run_local.sh
+++ run_local.sh
@@ -16,7 +16,7 @@ if command -v bashio >/dev/null 2>&1; then
 else
     echo "bashio not found, using default values"
     # Provide fallback or default values
-    OPENAI_API_KEY=${OPENAI_API_KEY:-"sk-dkv9oMuyXZ9YXJGaACj4TLIIB2gHuTS7Hu83Y-pRXzT3BlbkFJYhW7_XMFMyxeP05rO708BMF18z3beuWiK_5wgXrt8A"}
+    OPENAI_API_KEY=${OPENAI_API_KEY:-"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"}
     PORT=${PORT:-8087}
 fi
 
@@ -26,9 +26,11 @@ echo "PORT: ${PORT}"
 
 # Set FLASK_APP environment variable
 export FLASK_APP=haos_addon/app.py
+export OPENAI_API_KEY=${OPENAI_API_KEY}
 
 # Debug statement to confirm FLASK_APP is set
 echo "FLASK_APP is set to ${FLASK_APP}"
+echo "APIKEY is set to ${OPENAI_API_KEY}"
 
 # Run the Flask app with the specified port
 echo "Trying Flask app on port ${PORT}..."
